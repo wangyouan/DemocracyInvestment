b2a0ca0a4385a7bebe811cb3a1ba2ee70fec561e
Mark Wang <wangyouan0629@hotmail.com>
2020-05-12 19:51:13 +0800

    Initial Update

---
+++ Constants/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Filename: __init__.py
+# @Date: 2020/5/12
+# @Author: Mark Wang
+# @Email: wangyouan@gamil.com
+
+from Constants.path_info import PathInfo
+
+class Constants(PathInfo):
+    # common identifier
+    YEAR = 'year'
+    CUSIP = 'cusip'
+    TICKER = 'tic'
+    CUSIP8 = 'cusip8'
+    CUSIP6 = 'cusip6'
+    GVKEY = 'gvkey'
+    CIK = 'cik'
+
+
+    SIC_CODE = 'sic'
---
+++ Constants/path_info.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Filename: path_info
+# @Date: 2020/5/12
+# @Author: Mark Wang
+# @Email: wangyouan@gamil.com
+
+import os
+
+class PathInfo(object):
+    ROOT_PATH = '/home/zigan/Documents/wangyouan/research/DemocraryInvestment'
+
+    DATA_PATH = os.path.join(ROOT_PATH, 'data')
+    TEMP_PATH = os.path.join(ROOT_PATH, 'temp')
+    RESULT_PATH = os.path.join(ROOT_PATH, 'result')
+
+    PRE_ROOT_PATH = '/home/zigan/Documents/wangyouan/research/PoliticalConnections'
+    PRE_DATA_PATH = os.path.join(PRE_ROOT_PATH, 'data')
+    PRE_TEMP_PATH = os.path.join(PRE_ROOT_PATH, 'temp')
+    PRE_RESULT_PATH = os.path.join(PRE_ROOT_PATH, 'output')
+
+    STATA_PATH = os.path.join(ROOT_PATH, 'stata')
+    STATA_RESULT_PATH = os.path.join(STATA_PATH, 'result')
+    STATA_CODE_PATH = os.path.join(STATA_PATH, 'code')
+
+    DATABASE_PATH = '/home/zigan/Documents/wangyouan/database'
